a0d37aa838e149befee856007f5eaca1a246bed9
cw-dbhingradiya <dbhingradiya@codewinglet.com>
2024-01-08 12:35:53 +0530

    main

--- src/sections/mui/pagination-view.jsx
+++ src/sections/mui/pagination-view.jsx
@@ -3,59 +3,46 @@ import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
 import { Typography } from "@mui/material";
 
+import Masonry from "@mui/lab/Masonry";
+import Container from "@mui/material/Container";
 import PaginationItem from "@mui/material/PaginationItem";
+
+import ComponentBlock from "../component-block";
 
 export default function BasicPagination() {
   return (
-    <Stack gap={10}>
-      <Stack spacing={5}>
-        <Typography variant="h4">Circular</Typography>
-        <Pagination count={10} color="primary" />
-        <Pagination
-          count={10}
-          color="primary"
-          variant="outlined"
-          renderItem={(item) => <PaginationItem {...item} />}
-        />
-        <Pagination count={10} color="primary" />
-      </Stack>
+    <>
+      <Container sx={{ my: 10 }}>
+        <Masonry columns={{ xs: 1, md: 2 }} spacing={5}>
+          <ComponentBlock title="Circular">
+            <Pagination count={10} color="primary" />
+            <Pagination count={10} color="primary" variant="outlined" renderItem={(item) => <PaginationItem {...item} />} />
+            <Pagination count={10} color="primary" />
+          </ComponentBlock>
 
-      <Stack spacing={5}>
-        <Typography variant="h4">Rounded</Typography>
-        <Pagination count={10} color="primary" shape="rounded" />
-        <Pagination
-          count={10}
-          color="primary"
-          variant="outlined"
-          shape="rounded"
-        />
-        <Pagination count={10} color="primary" variant="soft" shape="rounded" />
-      </Stack>
+          <ComponentBlock title="Rounded">
+            <Pagination count={10} color="primary" shape="rounded" />
+            <Pagination count={10} color="primary" variant="outlined" shape="rounded" />
+            <Pagination count={10} color="primary" variant="soft" shape="rounded" />
+          </ComponentBlock>
 
-      <Stack spacing={5}>
-        <Typography variant="h4">Size</Typography>
-        <Pagination count={10} color="primary" shape="rounded" size="small" />
-        <Pagination
-          count={10}
-          color="primary"
-          variant="outlined"
-          shape="rounded"
-        />
-        <Pagination
-          count={10}
-          color="primary"
-          variant="soft"
-          shape="rounded"
-          size="large"
-        />
-      </Stack>
+          <ComponentBlock title="Size + Rounded">
+            <Stack spacing={5}>
+              <Pagination count={10} color="primary" shape="rounded" size="small" />
+              <Pagination count={10} color="primary" variant="outlined" shape="rounded" />
+              <Pagination count={10} color="primary" variant="soft" shape="rounded" size="large" />
+            </Stack>
+          </ComponentBlock>
 
-      <Stack spacing={5}>
-        <Typography variant="h4">Size</Typography>
-        <Pagination count={10} color="primary" size="small" />
-        <Pagination count={10} color="primary" variant="outlined" />
-        <Pagination count={10} color="primary" variant="soft" size="large" />
-      </Stack>
-    </Stack>
+          <ComponentBlock title="Size + Circular">
+            <Stack spacing={5}>
+              <Pagination count={10} color="primary" size="small" />
+              <Pagination count={10} color="primary" variant="outlined" />
+              <Pagination count={10} color="primary" variant="soft" size="large" />
+            </Stack>
+          </ComponentBlock>
+        </Masonry>
+      </Container>
+    </>
   );
 }
